7791b28947bf8ed46c6536b969ed633e13d17c62
bxqm803 <142374785+bxqm803@users.noreply.github.com>
2026-05-23 05:13:25 -0600

    Update modeling_llama_add_attn.py

--- model_zoo/llama/modeling_llama_add_attn.py
+++ model_zoo/llama/modeling_llama_add_attn.py
@@ -168,9 +168,7 @@ class RMSNorm(nn.Module):
     def __init__(self, hidden_size, eps=1e-6):
         super().__init__()
         self.weight = nn.Parameter(torch.ones(hidden_size))
-        self.variance_epsilon = epsinit__(self, hidden_size, eps=1e-6):
-        super().__init__()
-        self.weight = nn.Parameter(torch.ones
+        self.variance_epsilon = eps
 
     def forward(self, hidden_states):
         variance = hidden_states.to(torch.float32).pow(2).mean(
